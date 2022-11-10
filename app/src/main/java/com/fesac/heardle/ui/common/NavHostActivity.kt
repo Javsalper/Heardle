176a43ea4b73e10c2de5d9dd2cfc6ac8aed68fae
javsalper <salguero_javi@hotmail.com>
2022-11-10 12:15:19 +0100

    clase 8 Noviembre

---
+++ app/src/main/java/com/fesac/heardle/ui/common/NavHostActivity.kt
@@ -0,0 +1,12 @@
+package com.fesac.heardle.ui.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fesac.heardle.R
+
+class NavHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
